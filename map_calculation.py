ba1cef6fef68f4d4ba7f40512c34f77fbb493104
Shah-imran <shahimrans64@gmail.com>
2023-06-09 07:00:46 +0600

    generated output for 1st exp

--- map_calculation.py
+++ map_calculation.py
@@ -3,14 +3,49 @@ from PIL import Image
 import torch
 import detect_mod
 from pprint import pprint
+import pandas as pd
+import torch
+import numpy as np
 from torchmetrics.detection.mean_ap import MeanAveragePrecision
 
-detection_config = vars(detect_mod.parse_opt())
 
-detection_model = detect_mod.warmup_model(weights=detection_config['weights'],
-                                            data=detection_config['data'])
-for key in ['weights', 'data', 'imgsz', 'device', 'half', 'dnn', 'source']:
-    detection_config.pop(key)
+def save_maps(data_dict):
+    mod_data = []
+    random_data = []
+
+    for path, values in data_dict.items():
+        # Convert the tensors to standard python data types
+        map_val = values['map'].item()
+        map_50_val = values['map_50'].item()
+        map_75_val = values['map_75'].item()
+
+        # Construct a dictionary for the row
+        row = {'map': map_val, 'map_50': map_50_val, 'map_75': map_75_val}
+
+        if 'mod' in path or 'original' in path:
+            # Extract the ratio from the path, or use NaN if it's the 'original' path
+            ratio = float(path.split('/')[-2]) if 'mod' in path else np.nan
+            row['ratio'] = ratio
+            mod_data.append(row)
+        elif 'random' in path:
+            # Extract the ratio from the path
+            ratio = float(path.split('/')[-2])
+            row['ratio'] = ratio
+            random_data.append(row)
+
+    # Create the DataFrames
+    mod_df = pd.DataFrame(mod_data)
+    random_df = pd.DataFrame(random_data)
+
+    # Merge the two DataFrames on 'ratio'
+    merged_df = pd.merge(mod_df, random_df, on='ratio', suffixes=('_mod', '_random'), how='outer')
+    merged_df = merged_df.sort_values('ratio')
+    # Save the DataFrame to an Excel file
+    merged_df.to_excel('output.xlsx', index=False)
+
+    # Show the merged DataFrame
+    print(merged_df)
+
 
 def load_data(image_dir, label_dir):
     # Sorted list of image and label files
@@ -92,30 +127,38 @@ def calculate_map(image_path):
     metric.update(preds, targets)
     return metric.compute()
 
-image_path = "./runs/experiment/original/image/"
 
-maps = {image_path: calculate_map(image_path)} 
+if __name__=="__main__":
+    detection_config = vars(detect_mod.parse_opt())
+    detection_model = detect_mod.warmup_model(weights=detection_config['weights'],
+                                                data=detection_config['data'])
+    for key in ['weights', 'data', 'imgsz', 'device', 'half', 'dnn', 'source']:
+        detection_config.pop(key)
 
-base_path = f"./runs/experiment/mod/"
+    image_path = "./runs/experiment/original/image/"
 
-for subdir in os.listdir(base_path):
-    image_path = f"{base_path}{subdir}/"
+    maps = {image_path: calculate_map(image_path)} 
 
-    maps.get(image_path, {})
-    maps[image_path] = calculate_map(image_path)
+    base_path = f"./runs/experiment/mod/"
 
-base_path = f"./runs/experiment/random/"
+    for subdir in os.listdir(base_path):
+        image_path = f"{base_path}{subdir}/"
 
-for subdir in os.listdir(base_path):
-    image_path = f"{base_path}{subdir}/"
+        maps.get(image_path, {})
+        maps[image_path] = calculate_map(image_path)
 
-    maps.get(image_path, {})
-    maps[image_path] = calculate_map(image_path)
+    base_path = f"./runs/experiment/random/"
 
-pprint(maps)
+    for subdir in os.listdir(base_path):
+        image_path = f"{base_path}{subdir}/"
 
-# keys = sorted([ item for item in maps.keys()])
+        maps.get(image_path, {})
+        maps[image_path] = calculate_map(image_path)
 
-# for key in keys:
-#     print(key)
-#     pprint(maps[key])
+    save_maps(maps)
+
+    # keys = sorted([ item for item in maps.keys()])
+
+    # for key in keys:
+    #     print(key)
+    #     pprint(maps[key])
